1bd79053d0be1d37df1f23f5a336ec69fa875fe9
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-10-23 17:18:23 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,16 @@
 package org.example;
 
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-// import io.github.bonigarcia.wdm.WebDriverManager;
-// import org.openqa.selenium.By;
-// import org.openqa.selenium.Keys;
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.WebElement;
-// import org.openqa.selenium.chrome.ChromeDriver;
-// import org.openqa.selenium.chrome.ChromeOptions;
-// import org.openqa.selenium.support.events.WebDriverListener;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.events.WebDriverListener;
 
 // import io.github.bonigarcia.wdm.WebDriverManager;
 public class Main {
@@ -28,24 +28,24 @@ public class Main {
 
         // // Close the browser
         // driver.quit();
-        // System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        // ChromeOptions options = new ChromeOptions();
-        // options.addArguments("--headless");
-        // options.addArguments("start-maximized");
-        // options.addArguments("disable-infobars");
-        // options.addArguments("--disable-extensions");
-        // options.addArguments("--disable-dev-shm-usage");
-        // options.addArguments("--no-sandbox");
-        // WebDriverManager.chromedriver().setup();
-        // WebDriver driver = new ChromeDriver();
-        // driver.get("https://www.google.com/");
-        // WebElement searchInput = driver.findElement(By.xpath("//textarea"));
-        // searchInput.sendKeys("selenium");
-        // searchInput.sendKeys(Keys.RETURN);
-        // WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]"));
-        // String fetchedTitle=title.getText();
-        // System.out.println(fetchedTitle);
-        // driver.quit();
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("start-maximized");
+        options.addArguments("disable-infobars");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        WebElement searchInput = driver.findElement(By.xpath("//textarea"));
+        searchInput.sendKeys("selenium");
+        searchInput.sendKeys(Keys.RETURN);
+        WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]"));
+        String fetchedTitle=title.getText();
+        System.out.println(fetchedTitle);
+        driver.quit();
        System.out.println("Hello World");
     }
 }
