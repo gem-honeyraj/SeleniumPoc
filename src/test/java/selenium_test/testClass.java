df6aeac826074b142d98c12651296d5165c7187a
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-10-23 22:28:22 +0530

    Update testClass.java

--- src/test/java/selenium_test/testClass.java
+++ src/test/java/selenium_test/testClass.java
@@ -1,38 +1,39 @@
 package selenium_test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.Test;
+// import io.github.bonigarcia.wdm.WebDriverManager;
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.Keys;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.WebElement;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.chrome.ChromeOptions;
+// import org.testng.annotations.Test;
 
 public class testClass {
 
     @Test
    public void demo()
    {
-               System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("start-maximized");
-        options.addArguments("disable-infobars");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
-        WebElement searchInput = driver.findElement(By.xpath("//textarea"));
-        searchInput.sendKeys("selenium");
-        searchInput.sendKeys(Keys.RETURN);
-        WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]"));
-        String fetchedTitle=title.getText();
-        System.out.println(fetchedTitle);
+       //         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+       //  ChromeOptions options = new ChromeOptions();
+       //  options.addArguments("--headless");
+       //  options.addArguments("start-maximized");
+       //  options.addArguments("disable-infobars");
+       //  options.addArguments("--disable-extensions");
+       //  options.addArguments("--disable-dev-shm-usage");
+       //  options.addArguments("--no-sandbox");
+       //  WebDriverManager.chromedriver().setup();
+       //  WebDriver driver = new ChromeDriver();
+       //  driver.get("https://www.google.com/");
+       //  WebElement searchInput = driver.findElement(By.xpath("//textarea"));
+       //  searchInput.sendKeys("selenium");
+       //  searchInput.sendKeys(Keys.RETURN);
+       //  WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]"));
+       //  String fetchedTitle=title.getText();
+       //  System.out.println(fetchedTitle);
+       // System.out.println("hello");
+        // driver.quit();
        System.out.println("hello");
-        driver.quit();
 
    }
 }
