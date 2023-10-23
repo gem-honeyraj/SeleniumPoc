e94645b04611c8af25ee43daf48f4b08f1ea94e9
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-10-23 15:20:56 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,14 +3,14 @@ package org.example;
 // import org.openqa.selenium.WebDriver;
 // import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.events.WebDriverListener;
+// import io.github.bonigarcia.wdm.WebDriverManager;
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.Keys;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.WebElement;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.chrome.ChromeOptions;
+// import org.openqa.selenium.support.events.WebDriverListener;
 
 // import io.github.bonigarcia.wdm.WebDriverManager;
 public class Main {
